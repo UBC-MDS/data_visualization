9303c4299d592bf01c846e327b553873c9e0e0c3
irene93 <irene93@student.ubc.ca>
2022-01-14 13:14:43 -0800

    Update method docstring

--- src/data_visualization/histogram.py
+++ src/data_visualization/histogram.py
@@ -1,5 +1,5 @@
-def histogram(df, x, y, facet=False):
-    """Plot a histogram with the color scheme magma and an option to facet.
+def histogram(df, x, y):
+    """Plot a histogram with the color scheme magma
     Parameters
     ----------
     df : dataframe
@@ -8,14 +8,12 @@ def histogram(df, x, y, facet=False):
         Column name of the numerical variable to be plotted on the x-axis
     y : string
         An aggregation function to be plotted on the y-axis
-    facet : boolean
-        Determines whether seprate graphs will be created for each category
     Returns
     -------
     altair.vegalite.v4.api.Chart
         Boxplot displaying distribution of categorical variables with/without faceting
     Examples
     --------
-    >>> from data_visualization.data_visualization import boxplot
-    >>> plot_words(mtcars, horsepower, cars, facet=True)
+    >>> from data_visualization.data_visualization import histogram
+    >>> plot(mtcars, "cars", "count()")
     """
