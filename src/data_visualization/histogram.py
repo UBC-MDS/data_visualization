345c3905722acac2f627daa2462cd6cc21a1e901
irene93 <irene93@student.ubc.ca>
2022-01-14 17:17:11 -0800

    Modify docstring

--- src/data_visualization/histogram.py
+++ src/data_visualization/histogram.py
@@ -1,19 +1,22 @@
 def histogram(df, x, y):
     """Plot a histogram with the color scheme magma
+
     Parameters
     ----------
     df : dataframe
         Dataframe containing the variables for plotting
     x : string
-        Column name of the numerical variable to be plotted on the x-axis
+        Column name of the variable to be plotted on the x-axis
     y : string
         An aggregation function to be plotted on the y-axis
+
     Returns
     -------
     altair.vegalite.v4.api.Chart
-        Boxplot displaying distribution of categorical variables with/without faceting
+        A histogram displaying distribution based on the aggregation function
+
     Examples
     --------
-    >>> from data_visualization.data_visualization import histogram
-    >>> plot(mtcars, "cars", "count()")
+    >>> from magmaviz.magmaviz import histogram
+    >>> histogram(mtcars, "cars", "count()")
     """
